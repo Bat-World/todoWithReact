78c72e16701426cbab8c94c78c4c9f64007be358
1A-engineer <drewuncle863724lu@gmail.com>
2025-01-02 12:15:33 +0800

    design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-
-import './App.css';
-import { v4 as uuidv4 } from 'uuid';
-import React, {useState} from "react"
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import React, { useState } from "react";
 
 function App() {
-
   const [todo, Settodo] = useState([]);
-  const [ inputValue, SetinputValue] = useState("");
- 
+  const [inputValue, SetinputValue] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-       hello
-      </header>
-     <div id='container'>
-      <h1>To-Do list</h1>
-        <input id='inputBox' placeholder='Add task here'/>
-        <button id='addButton'>Add</button>
+      <header className="App-header">hello</header>
+      <div id="container">
+        <div id="upperSection">
+          <h1 id="title">To-Do list</h1>
+          <div id="inputSection">
+            <input id="inputBox" placeholder="Add a new task ..." />
+            <button id="addButton">Add</button>
+          </div>
+          <div id="taskStatusSection">
+            <button id="allTasksButton">All</button>
+            <button id="activeTasksButton">Active</button>
+            <button id="completedTasksButton">Copmleted</button>
+          </div>
+          <div id="numberOfTasksSection">
+            <p id="numberOfTasksP">No tasks yet. Add one above!</p>
+          </div>
         </div>
+
+        {/* <div id="lowerSection">
+          <p>Powered By</p>
+        </div> */}
+      </div>
     </div>
   );
 }
