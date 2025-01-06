4a155fd6863231e29b3f58e346c4d5b76110fd6a
Second <drewuncle863724lu@gmail.com>
2025-01-07 01:56:40 +0800

    a whole new design

--- src/App.js
+++ src/App.js
@@ -18,12 +18,13 @@ function App() {
   const [deleteTask, setDeleteTask] = useState();
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-
+  // Theme switch
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
     document.body.classList.toggle("darkmode", !isDarkMode);
   };
 
+  // Task status filter
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
@@ -79,31 +80,54 @@ function App() {
     setFilterState(state);
   };
 
-
   // Delete task logic
   const handleDeleteTask = (id) => {
     // Filter out the task with the specified id
     const currentTodos = todos.filter((todo) => todo.id !== id);
-  
+
     // Update the todos state
     setTodos(currentTodos);
-  
+
     // Update the overall task count
     setNumberOfTasks(numberOfTasks - 1);
-  
+
     // Update the completed tasks count if the deleted task was completed
     const deletedTask = todos.find((todo) => todo.id === id);
     if (deletedTask && deletedTask.status === "COMPLETED") {
       setNumberOfCompletedTasks(numOfCompletedTasks - 1);
     }
   };
-  
 
   return (
-    <div className="App">
+    <div >
+      <div id="header">
+      <p id="big-title">Productive Pal</p>
       <button id="theme-switch" onClick={toggleTheme}>
-            {isDarkMode ? "🌙 Dark Mode" : "☀️ Light Mode"}
-          </button>
+        {isDarkMode ? (
+          <svg
+            id="lightmode-button"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e8eaed"
+          >
+            <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#666666"
+          >
+            <path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z" />
+          </svg>
+        )}
+      </button>
+      </div>
+   
       <div id="container">
         <div id="upper-section">
           <h1 id="title">To-Do List</h1>
@@ -117,7 +141,9 @@ function App() {
               autoComplete="off"
             />
             {error.length > 1 && <div>{error}</div>}
-            <button id="add-button" onClick={handleAddButtonClick}>
+            <button id="add-button" 
+            onClick={handleAddButtonClick}
+            style={{cursor: "pointer"}}>
               Add
             </button>
           </div>
@@ -127,6 +153,7 @@ function App() {
             id="all-tasks-button"
             onClick={() => handleFilterState("All")}
             className={filterState === "All" ? "active-filter" : ""}
+            style={{cursor: "pointer"}}
           >
             All
           </button>
@@ -134,6 +161,7 @@ function App() {
             id="active-tasks-button"
             onClick={() => handleFilterState("ACTIVE")}
             className={filterState === "ACTIVE" ? "active-filter" : ""}
+            style={{cursor: "pointer"}}
           >
             Active
           </button>
@@ -141,6 +169,7 @@ function App() {
             id="completed-tasks-button"
             onClick={() => handleFilterState("COMPLETED")}
             className={filterState === "COMPLETED" ? "active-filter" : ""}
+            style={{cursor: "pointer"}}
           >
             Completed
           </button>
@@ -156,8 +185,9 @@ function App() {
               }
             })
             .map((todo, index) => (
-              <div id="list-container" key={todo.id}>
+              <div id="list-container" key={todo.id} >
                 <input
+                 style={{cursor: "pointer"}}
                   id="check-box"
                   type="checkbox"
                   checked={todo.status === "COMPLETED"}
@@ -175,6 +205,7 @@ function App() {
                 <button
                   id="delete-button"
                   onClick={() => handleDeleteTask(todo.id)}
+                 style={{cursor: "pointer"}}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
