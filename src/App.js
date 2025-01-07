005f2d32db311b26f7d5a830580e4569087e4c0c
1A-engineer <drewuncle863724lu@gmail.com>
2025-01-07 09:39:49 +0800

    final version maybe

--- src/App.js
+++ src/App.js
@@ -82,52 +82,73 @@ function App() {
 
   // Delete task logic
   const handleDeleteTask = (id) => {
-    // Filter out the task with the specified id
+
     const currentTodos = todos.filter((todo) => todo.id !== id);
 
-    // Update the todos state
     setTodos(currentTodos);
 
-    // Update the overall task count
     setNumberOfTasks(numberOfTasks - 1);
 
-    // Update the completed tasks count if the deleted task was completed
     const deletedTask = todos.find((todo) => todo.id === id);
     if (deletedTask && deletedTask.status === "COMPLETED") {
       setNumberOfCompletedTasks(numOfCompletedTasks - 1);
     }
   };
 
+  
+  // Clearcompleted
+
+  const handleClearCompleted = () => {
+    const activeTodos = todos.filter((todo) => todo.status !== "COMPLETED");
+    setTodos(activeTodos);
+    setNumberOfTasks(activeTodos.length);
+    setNumberOfCompletedTasks(0);
+  };
+  
+
   return (
-    <div >
+    <div>
       <div id="header">
-      <p id="big-title">Productive Pal</p>
-      <button id="theme-switch" onClick={toggleTheme}>
-        {isDarkMode ? (
-          <svg
-            id="lightmode-button"
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#e8eaed"
-          >
-            <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z" />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#666666"
-          >
-            <path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z" />
-          </svg>
-        )}
-      </button>
+        <p id="big-title">Productive Pal</p>
+        <svg
+          id="leaf-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          width="33"
+          height="33"
+          viewBox="0 0 612 612"
+        >
+          <path
+            fill="#9CB53B"
+            d="M106.542 508.401s-72.24-172.822 57.245-301.715C281.437 89.707 458.928 71.603 489.735 70.749c0 0 47.667 156.435-71.334 333.785C299.524 581.93 134.633 533.793 134.633 533.793s162.888-207.637 143.2-206.383c-13.664.819-99.156 94.923-171.291 180.991"
+          />
+        </svg>
+
+        <button id="theme-switch" onClick={toggleTheme}>
+          {isDarkMode ? (
+            <svg
+              id="lightmode-button"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e8eaed"
+            >
+              <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#666666"
+            >
+              <path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z" />
+            </svg>
+          )}
+        </button>
       </div>
-   
+
       <div id="container">
         <div id="upper-section">
           <h1 id="title">To-Do List</h1>
@@ -141,9 +162,11 @@ function App() {
               autoComplete="off"
             />
             {error.length > 1 && <div>{error}</div>}
-            <button id="add-button" 
-            onClick={handleAddButtonClick}
-            style={{cursor: "pointer"}}>
+            <button
+              id="add-button"
+              onClick={handleAddButtonClick}
+              style={{ cursor: "pointer" }}
+            >
               Add
             </button>
           </div>
@@ -153,7 +176,7 @@ function App() {
             id="all-tasks-button"
             onClick={() => handleFilterState("All")}
             className={filterState === "All" ? "active-filter" : ""}
-            style={{cursor: "pointer"}}
+            style={{ cursor: "pointer" }}
           >
             All
           </button>
@@ -161,7 +184,7 @@ function App() {
             id="active-tasks-button"
             onClick={() => handleFilterState("ACTIVE")}
             className={filterState === "ACTIVE" ? "active-filter" : ""}
-            style={{cursor: "pointer"}}
+            style={{ cursor: "pointer" }}
           >
             Active
           </button>
@@ -169,7 +192,7 @@ function App() {
             id="completed-tasks-button"
             onClick={() => handleFilterState("COMPLETED")}
             className={filterState === "COMPLETED" ? "active-filter" : ""}
-            style={{cursor: "pointer"}}
+            style={{ cursor: "pointer" }}
           >
             Completed
           </button>
@@ -185,9 +208,9 @@ function App() {
               }
             })
             .map((todo, index) => (
-              <div id="list-container" key={todo.id} >
+              <div id="list-container" key={todo.id}>
                 <input
-                 style={{cursor: "pointer"}}
+                  style={{ cursor: "pointer" }}
                   id="check-box"
                   type="checkbox"
                   checked={todo.status === "COMPLETED"}
@@ -205,7 +228,7 @@ function App() {
                 <button
                   id="delete-button"
                   onClick={() => handleDeleteTask(todo.id)}
-                 style={{cursor: "pointer"}}
+                  style={{ cursor: "pointer" }}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -229,6 +252,7 @@ function App() {
               {numOfCompletedTasks} of {numberOfTasks} tasks completed
             </p>
           )}
+          <button id="clearCompleted-button"   onClick={handleClearCompleted}>Clear Completed</button>
         </div>
       </div>
     </div>
